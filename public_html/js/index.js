bdc22640facbcfb6fbd100c2614f918be379e19c
omaralm <omar.al-mashhadani@metropolia.fi>
2021-04-28 16:22:43 +0300

    update home page index.html, index.js

--- public_html/js/index.js
+++ public_html/js/index.js
@@ -1,94 +1,65 @@
-'use strict';
-const picArray = [
-  {
-    'title': 'Cat',
-    'price': '1000€',
-    'breed': 'Breed',
-    'location': 'Address',
-    'viewbtn': 'View',
-    'filename': 'img/pic1.png',
-  },
-  {
-
-    'title': 'Dog',
-    'price': '800€',
-    'breed': 'Breed',
-    'location': 'Address',
-    'viewbtn': 'View',
-    'filename': 'img/pic2.png',
-  },
-  {
-    'title': 'Puppy',
-    'price': '990€',
-    'breed': 'Breed',
-    'location': 'Address',
-    'viewbtn': 'View',
-    'filename': 'img/pic3.png',
-  },
-  {
-    'title': 'Cat',
-    'price': '1000€',
-    'breed': 'Breed',
-    'location': 'Address',
-    'viewbtn': 'View',
-    'filename': 'img/pic1.png',
-  },
-  {
-
-    'title': 'Dog',
-    'price': '800€',
-    'breed': 'Breed',
-    'location': 'Address',
-    'viewbtn': 'View',
-    'filename': 'img/pic2.png',
-  },
-  {
-    'title': 'Puppy',
-    'price': '990€',
-    'breed': 'Breed',
-    'location': 'Address',
-    'viewbtn': 'View',
-    'filename': 'img/pic3.png',
-  },
-];
+const url = 'http://localhost:3000'; // change url when uploading to server
 
 const grid = document.querySelector('.grid-col1');
 
-picArray.forEach((file) => {
+const createPetCards = (pets) => {
 
-  const h2 = document.createElement('h2');
-  const figure = document.createElement('figure');
-  const price = document.createElement('div');
-  price.className = 'price';
-  const h3 = document.createElement('h3');
-  const img = document.createElement('img');
-  const p = document.createElement('p');
-  const button = document.createElement('button');
+  pets.forEach((pet) => {
 
-  button.style.textTransform = 'uppercase';
-  button.style.padding = '8px 24px';
-  button.style.marginLeft = '10px';
-  button.style.marginBottom = '15px';
+    const img = document.createElement('img');
+    img.src = url + '/' + pet.picture;
+    img.alt = pet.name;
+    img.classList.add('resp');
 
-  h2.innerText = file.title;
-  price.innerText= file.price;
-  p.innerText = file.location;
-  button.innerText = file.viewbtn;
-  h3.innerText = file.breed;
+    const h2 = document.createElement('h2');
+    h2.innerText = pet.title;
 
-  img.src = file.filename;
-  img.alt = file.title;
+    const figure = document.createElement('figure');
+    const price = document.createElement('div');
+    price.className = 'price';
+    price.innerText= pet.price;
 
-  figure.appendChild(h2);
-  figure.appendChild(img);
-  figure.appendChild(h3);
-  figure.appendChild(price);
-  figure.appendChild(p);
-  figure.appendChild(button);
+    const h3 = document.createElement('h3');
+    h3.innerText = pet.breed;
 
-  //main.appendChild(article);
-  grid.appendChild(figure);
-});
+    //location!?
+    // const p = document.createElement('p');
+    // p.innerText = file.location;
+
+    // view button
+    const viewButton = document.createElement('button');
+    viewButton.innerText = 'view pet';
+
+    viewButton.style.textTransform = 'uppercase';
+    viewButton.style.padding = '8px 24px';
+    viewButton.style.marginLeft = '10px';
+    viewButton.style.marginBottom = '15px';
+
+    figure.appendChild(h2);
+    figure.appendChild(img);
+    figure.appendChild(h3);
+    figure.appendChild(price);
+    // figure.appendChild(p);
+    figure.appendChild(viewButton);
+
+    //main.appendChild(article);
+    grid.appendChild(figure);
+  });
+};
+
+// AJAX call
+const getPet = async () => {
+  try {
+    const response = await fetch(url + '/pet');
+    const pets = await response.json();
+    createPetCards(pets);
+  }
+  catch (e) {
+    console.log(e.message);
+  }
+};
+getPet();
+
 
 // PopUp
 
@@ -98,5 +69,3 @@ document.querySelector("#show-login").addEventListener("click",function(){
 document.querySelector(".popup .close-btn").addEventListener("click",function(){
   document.querySelector(".popup").classList.remove("active");
 });
-
-
